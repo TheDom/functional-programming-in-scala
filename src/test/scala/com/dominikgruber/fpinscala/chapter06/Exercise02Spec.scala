0bad48f23dfdc12e331bf7865b75dabeca30fa2a
Dominik Gruber <mail@dominikgruber.com>
2014-04-01 20:12:18 +0200

    Chapter 6, Exercise 2

---
+++ src/test/scala/com/dominikgruber/fpinscala/chapter06/Exercise02Spec.scala
@@ -0,0 +1,10 @@
+package com.dominikgruber.fpinscala.chapter06
+
+import org.scalatest._
+
+class Exercise02Spec extends FlatSpec with Matchers {
+
+  "double" should "generate a double" in {
+    Exercise02.double(Simple(-10000000))._1 should be (0.3733188882470131)
+  }
+}
